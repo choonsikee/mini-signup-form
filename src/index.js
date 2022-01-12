cc5743feaa2b91fc1d0be4e39ed418a2443e12d3
choonsikee <hanameee@naver.com>
2022-01-12 18:59:45 +0900

    feat: 요구사항 1. autofocus 구현

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-// TODO: 이 곳에 정답 코드를 작성해주세요.
+// 1.  autofocus 구현
+// 대상: ID 입력 input
+// 시점: 페이지(window)가 로드 되었을 때
+// 이벤트: Focus()
+const $id = document.getElementById('id')
+window.addEventListener('load', () => $id.focus())
